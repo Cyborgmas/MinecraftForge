bbd217ed8f782ea5e42cc109ea089106938a867b
Cyborgmas <cyborgmas18@gmail.com>
2020-09-28 13:44:29 -0400

    licenses

--- src/main/java/net/minecraftforge/client/ISkyRenderHandler.java
+++ src/main/java/net/minecraftforge/client/ISkyRenderHandler.java
@@ -16,7 +16,6 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
-
 package net.minecraftforge.client;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
