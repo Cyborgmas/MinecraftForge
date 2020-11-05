22f112bf49e50df2a6b27deb24b4d3ba89743dd5
Cyborgmas <cyborgmas18@gmail.com>
2020-11-05 08:58:18 -0500

    update to new mappings, add license

--- src/main/java/net/minecraftforge/client/gui/ExperimentalSettingsConfirmationScreen.java
+++ src/main/java/net/minecraftforge/client/gui/ExperimentalSettingsConfirmationScreen.java
@@ -1,3 +1,22 @@
+/*
+ * Minecraft Forge
+ * Copyright (c) 2016-2020.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation version 2.1
+ * of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 package net.minecraftforge.client.gui;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
@@ -25,13 +44,13 @@ public class ExperimentalSettingsConfirmationScreen extends ConfirmScreen
     }
 
     @Override
-    protected void func_231160_c_()
+    protected void init()
     {
-        super.func_231160_c_();
-        this.field_230710_m_.remove(0);
-        this.field_230705_e_.remove(0);
+        super.init();
+        this.buttons.remove(0);
+        this.children.remove(0);
         //remove and re-add the same button but with an extra on click action.
-        this.func_230480_a_(new Button(this.field_230708_k_ / 2 - 155, this.field_230709_l_ / 6 + 96, 150, 20, this.confirmButtonText, (button) ->
+        this.addButton(new Button(this.width / 2 - 155, this.height / 6 + 96, 150, 20, this.confirmButtonText, (button) ->
         {
             if(checkbox.isChecked())
             {
@@ -41,15 +60,15 @@ public class ExperimentalSettingsConfirmationScreen extends ConfirmScreen
             this.callbackFunction.accept(true);
         }));
 
-        checkBoxText = IBidiRenderer.func_243258_a(this.field_230712_o_, new TranslationTextComponent("forge.confirmgui.checkbox"), this.field_230708_k_ - 50);
-        this.checkbox = new CheckboxButton(this.field_230708_k_ / 2 - 10, this.field_230709_l_ / 6 + 156, 150, 20, StringTextComponent.field_240750_d_,false, false);
-        this.func_230480_a_(checkbox);
+        checkBoxText = IBidiRenderer.func_243258_a(this.font, new TranslationTextComponent("forge.confirmgui.checkbox"), this.width - 50);
+        this.checkbox = new CheckboxButton(this.width / 2 - 10, this.height / 6 + 156, 150, 20, StringTextComponent.EMPTY,false, false);
+        this.addButton(checkbox);
     }
 
     @Override
-    public void func_230430_a_(MatrixStack p_230430_1_, int p_230430_2_, int p_230430_3_, float p_230430_4_)
+    public void render(MatrixStack p_230430_1_, int p_230430_2_, int p_230430_3_, float p_230430_4_)
     {
-        super.func_230430_a_(p_230430_1_, p_230430_2_, p_230430_3_, p_230430_4_);
-        this.checkBoxText.func_241863_a(p_230430_1_, this.field_230708_k_ / 2, this.field_230709_l_ / 6 + 131);
+        super.render(p_230430_1_, p_230430_2_, p_230430_3_, p_230430_4_);
+        this.checkBoxText.func_241863_a(p_230430_1_, this.width / 2, this.height / 6 + 131);
     }
 }
